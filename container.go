f94e84aebf3dde0cc376dca613481fa600447100
Peter DeGroff <peter@roamz.com>
2013-06-17 01:02:11 +1000

    clarified mission statement

--- container.go
+++ container.go
@@ -1,5 +1,6 @@
 /*
-Alternative imagining of heap API. Rather than defining repetitive methods
+Alternative heap API with a focus on concurrent access and a simple
+interface that includes channel IO. Rather than defining repetitive methods
 for each type, create a heap with one function that acts as your heap.Less
 method. Interface with it through typical Push/Pop methods, or channels.
 pore.Heap is concurrency-safe and Pop blocks while waiting for Push
